6735d163bdcff38cb7e766f6a94d3f8b4aa36cc6
mrmarkolinus <72712091+mrmarkolinus@users.noreply.github.com>
2024-10-13 20:53:10 +0200

    first test written. MinecraftBinary implemented

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -3,7 +3,7 @@ use nbt_sniper::MinecraftBinary;
 #[test]
 fn bigtest() {
    
-   let mc_bin = MinecraftBinary::new("./files/bigtest.nbt".to_string());
+   let mc_bin = MinecraftBinary::new("tests/files/bigtest.nbt".to_string());
 
    mc_bin.format_output();
 }  
