13ddf689dfa585e96b00a66d81820623c9444079
mrmarkolinus <72712091+mrmarkolinus@users.noreply.github.com>
2024-10-16 22:17:52 +0200

    tested to_json

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,5 +4,5 @@ use nbt_sniper::NbtFile;
 fn bigtest() {
     let mc_bin = NbtFile::read("tests/files/bigtest.nbt".to_string());
     mc_bin.format_output();
-    mc_bin.to_json();
+    mc_bin.to_json("tests/files/output/out_bigtest.json");
 }
