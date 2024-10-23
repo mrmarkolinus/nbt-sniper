53cd2a8d635812bd2e3c3628fcee0de66d8e4c7c
mrmarkolinus <72712091+mrmarkolinus@users.noreply.github.com>
2024-10-23 22:10:45 +0200

    read regions files from world

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,6 +1,6 @@
 use crate::nbt;
 use crate::region;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 const REGION_PATH: &str = "region";
 const NETHER_PATH: &str = "DIM-1";
@@ -30,8 +30,12 @@ impl World {
         }
     }
 
-    pub fn read_world_directory(world_path: PathBuf) -> bool {
+    fn read_world_directory(world_path: PathBuf) -> bool {
         let mut valid_minecraft_world = true;
+
+        let mut overworld_regions = Vec::<region::RegionFile>::new();
+        let mut nether_regions = Vec::<region::RegionFile>::new();
+        let mut end_regions = Vec::<region::RegionFile>::new();
 
         if !world_path.is_dir() {
             valid_minecraft_world = false
@@ -42,14 +46,35 @@ impl World {
 
             if !overworld_region_path.is_dir() {
                 valid_minecraft_world = false;
+            } else {
+                Self::read_region_directory(overworld_region_path, &mut overworld_regions);
             }
             if !nether_region_path.is_dir() {
                 valid_minecraft_world = false;
+            } else {
+                Self::read_region_directory(nether_region_path, &mut nether_regions);
             }
             if !end_region_path.is_dir() {
                 valid_minecraft_world = false;
+            } else {
+                Self::read_region_directory(end_region_path, &mut end_regions);
             }
         }
         valid_minecraft_world
+    }
+
+    fn read_region_directory(region_path: PathBuf, regions: &mut Vec<region::RegionFile>) -> std::io::Result<()> {
+
+        match std::fs::read_dir(region_path) {
+            Ok(entries) => {
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        regions.push(region::RegionFile::read(entry.path()));     
+                    }
+                }
+            },
+            Err(_) => return Err(std::io::Error::new(std::io::ErrorKind::Other, "Error in reading the region files")),
+        }
+        Ok(())
     }
 }
